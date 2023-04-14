892b9f20ea6060cf565d2f588a79150f0eef9def
Seidgy Amano <seidgy@gmail.com>
2023-04-14 16:04:49 -0300

    Nav status

--- components/mainNav.vue
+++ components/mainNav.vue
@@ -1,12 +1,11 @@
 <template>
   <header class="main-nav" :neg="neg">
-    <nav>
+    <nav class="main-nav__nav" :class="{'resources': stateActiveSlide == 'resources', 'playbook': stateActiveSlide == 'playbook', 'map': stateActiveSlide == 'map'}">
       <li><a href="/" @click="openResource('intro-1')">Intro</a></li>
       <li><a href="#resources" @click="openResource('resources')">Resources</a></li>
       <li><a href="#playbook" @click="openResource('playbook')">Playbook</a></li>
       <li><a href="#map" @click="openResource('map')">Map</a></li>
     </nav>
-
       
   </header>
 </template>
@@ -66,10 +65,59 @@ const menuData = [
 nav {
   display: flex;
   gap: var(--s1);
+  position: relative;
 
   @media (max-width: 40em) {
     gap: var(--s0);
     align-items: center;
+  }
+
+  &::before {
+    content: '';
+    background-color: var(--primary-color);
+    height: 3px;
+    width: 128px;
+    position: absolute;
+    bottom: -3px;
+    left: -10px;
+    transition: all 0.2s ease-out;
+    @media (max-width: 40em) {
+      width: 87px;
+      left: -3px;
+    }
+  }
+}
+
+nav.resources {
+  &::before {
+    left: 128px;
+  }
+  @media (max-width: 40em) {
+    &::before {
+      left: 93px;
+    }
+  }
+}
+
+nav.playbook {
+  &::before {
+    left: 263px;
+  }
+  @media (max-width: 40em) {
+    &::before {
+      left: 191px;
+    }
+  }
+}
+
+nav.map {
+  &::before {
+    left: 400px;
+  }
+  @media (max-width: 40em) {
+    &::before {
+      left: 288px;
+    }
   }
 }
 
@@ -81,6 +129,13 @@ li a {
   color: var(--white-color);
   letter-spacing: .4px;
   text-decoration: none;
+  width: 110px;
+  display: inline-block;
+  text-align: center;
+
+  @media (max-width: 40em) {
+    width: 81px;
+  }
 }
 
 a:hover {
