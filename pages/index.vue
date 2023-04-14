711a1e4c747173e052f284271094c3046c1eefe9
Douglas Cantão <dcp.pereira@gmail.com>
2023-04-14 17:35:37 -0300

    CTJ-7 Setting overflow to map card on small screen

--- pages/index.vue
+++ pages/index.vue
@@ -84,11 +84,13 @@
         </div>
         <div v-if="stateActiveSlide == 'map'" class="slide grid-section" id="map">
           <div class="intro-section__content panel">
-            <stack-l>
-              <h2 class="color:secondary">College-to-Jobs Map</h2>
-              <p><strong>The College-to-Jobs Map visualizes how local worker trends align with college graduate growth in regions around the country.</strong> The tool allows users to dive into regional labor market changes and discover college graduate and workforce information. The map draws the connection between colleges, economic mobility, and regional economic development–presenting the graduate supply and labor demand challenges to building a prosperous and inclusive workforce.</p>
-              <p><base-button visual="primary" color="primary" el="a" href="https://collegetojobs.hks.harvard.edu/" target="_blank">Explore the Tool</base-button></p>
-            </stack-l>
+            <div class="scroll">
+              <stack-l>
+                <h2 class="color:secondary">College-to-Jobs Map</h2>
+                <p><strong>The College-to-Jobs Map visualizes how local worker trends align with college graduate growth in regions around the country.</strong> The tool allows users to dive into regional labor market changes and discover college graduate and workforce information. The map draws the connection between colleges, economic mobility, and regional economic development–presenting the graduate supply and labor demand challenges to building a prosperous and inclusive workforce.</p>
+                <p><base-button visual="primary" color="primary" el="a" href="https://collegetojobs.hks.harvard.edu/" target="_blank">Explore the Tool</base-button></p>
+              </stack-l>
+            </div>
             <a class="prev-link icon" @click="changeSlide('resources')">arrow_back</a>
           </div>
         </div>
@@ -166,6 +168,14 @@ section {
   }
 }
 
+#map {
+  @media (max-width: 32em) {
+    .intro-section__content > .scroll {
+      height: 50vh;
+      overflow-y: scroll;
+    }
+  }
+}
 #playbook,
 #resources {
   .intro-section__content {
